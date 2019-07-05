f251b39087800ca67beec083a4157dc7cbcfb758
southnort <south-nort@ya.ru>
2019-07-05 23:50:05 +0300

    * При компиляции создаётся один exe файл

--- XML Creator/Program.cs
+++ XML Creator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+
 
 namespace XML_Creator
 {
@@ -16,7 +18,25 @@ namespace XML_Creator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += 
+                new ResolveEventHandler(CurrentDomain_AssemblyResolve);
             Application.Run(new Form1());
         }
+
+        static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("XML_Creator.DotNetZip.dll"))
+            {
+                byte[] assemblyData = new byte[stream.Length];
+                stream.Read(assemblyData, 0, assemblyData.Length);
+                return Assembly.Load(assemblyData);
+            }
+
+
+        }
+
+
     }
+
+
 }
